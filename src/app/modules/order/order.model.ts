7725ef204d2e6bd406fcb6a9852d2361c1bf4339
Md Kamruzzaman <kamruzzaman-dev@users.noreply.github.com>
2023-12-18 17:41:15 +0600

    session issue resolved

--- src/app/modules/order/order.model.ts
+++ src/app/modules/order/order.model.ts
@@ -10,53 +10,62 @@ const ShipmentAddressSchema = new Schema({
   country: { type: String },
 });
 
-
-const OrdersSchema = new Schema<IOrders, OrdersModel>({
-  buyer_id: { type: Schema.Types.ObjectId, default: null },
-  user_id: {
-    type: Schema.Types.ObjectId,
-    ref: 'Route',
-    required: true,
-  },
-  order_code: { type: String, required: true, unique: true },
-  order_product_list: [
-    {
-      product_id: {
-        type: Schema.Types.ObjectId,
-        ref: 'Products',
-        required: true,
-      },
-      product_quantity: { type: Number, required: true },
-      product_price: { type: Number, required: true },
+const OrdersSchema = new Schema<IOrders, OrdersModel>(
+  {
+    buyer_id: { type: Schema.Types.ObjectId, default: null },
+    user_id: {
+      type: Schema.Types.ObjectId,
+      ref: 'Route',
+      required: true,
     },
-  ],
-  amount: { type: Number, required: true },
-  order_status: {
-    type: String,
-    enum: OrderStatus,
-    default: 'pending',
-    required: true,
+    order_code: { type: String, required: true, unique: true },
+    order_product_list: [
+      {
+        product_id: {
+          type: Schema.Types.ObjectId,
+          ref: 'Products',
+          required: true,
+        },
+        product_quantity: { type: Number, required: true },
+        product_price: { type: Number, required: true },
+      },
+    ],
+    total_items: { type: Number },
+    total_quantity: { type: Number },
+    amount: { type: Number, required: true },
+    order_status: {
+      type: String,
+      enum: OrderStatus,
+      default: 'pending',
+      required: true,
+    },
+    coupon_code: { type: String },
+    coupon_discount: { type: Number, default: 0 },
+    total_amount: { type: Number, required: true },
+    payment_id: { type: Schema.Types.ObjectId },
+    payment_status: {
+      type: String,
+      enum: PaymentStatus,
+      default: 'pending',
+      required: true,
+    },
+    payment_date: { type: String },
+    shipment_id: { type: Schema.Types.ObjectId },
+    shipment_status: {
+      type: String,
+      default: 'pending',
+      enum: ShipmentStatus,
+      required: true,
+    },
+    shipment_address: { type: ShipmentAddressSchema },
+    shipment_date: { type: String },
   },
-  coupon_code: { type: String },
-  coupon_discount: { type: Number, default: 0 },
-  total_amount: { type: Number, required: true },
-  payment_id: { type: Schema.Types.ObjectId },
-  payment_status: {
-    type: String,
-    enum: PaymentStatus,
-    default: 'pending',
-    required: true,
-  },
-  payment_date: { type: String },
-  shipment_id: { type: Schema.Types.ObjectId },
-  shipment_status: {
-    type: String,
-    default: 'pending',
-    enum: ShipmentStatus,
-    required: true,
-  },
-  shipment_address: { type: ShipmentAddressSchema }, 
-  shipment_date: { type: String },
-});
+  {
+    timestamps: true,
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
 
 export const Orders = model<IOrders, OrdersModel>('Orders', OrdersSchema);
--- src/app/modules/order/order.services.ts
+++ src/app/modules/order/order.services.ts
@@ -1,4 +1,5 @@
 import httpStatus from 'http-status';
+import mongoose from 'mongoose';
 import ApiError from '../../../errors/ApiError';
 import { Products } from '../products/products.model';
 import { IOrders, IOrdersReq } from './order.interface';
@@ -51,7 +52,8 @@ const createOrder = async (payload: IOrdersReq): Promise<IOrders | null> => {
     }
   }
 
-  //**[step-02] coupon checking // TODO: coupon functionality
+  //**[step-02] coupon checking
+  // TODO: coupon functionality
 
   //**[step-03] checking price calculation is right
   let total_price = 0;
@@ -69,24 +71,68 @@ const createOrder = async (payload: IOrdersReq): Promise<IOrders | null> => {
   //** [step-04] generate a order code
   const order_code = await generatedOrderCode(); // generated bus code
 
-  //** [step-05] create a order and reduce product quantity from product table   
-  // TODO: buyer is fake
-  // TODO: user_id will be update from auth middleware data
+  //** [step-05]  [reduce product quantity from product table]/part-01 and [create a order]/part-02
 
-  const result = await Orders.create({
-    buyer_id: '657ff528bcc34f2ba044d717',
-    user_id: '657ff528bcc34f2ba044d717',
-    order_code: order_code,
-    order_product_list: order_product_list,
-    amount,
-    total_amount: amount,
-    shipment_address,
-  });
+  const session = await mongoose.startSession();
 
-  if (!result) {
-    throw new ApiError(httpStatus.BAD_REQUEST, 'Order is not created');
+  try {
+    session.startTransaction();
+    // ** [reduce product quantity from product table]/part-01
+    for (const orderProduct of order_product_list) {
+      const productId = orderProduct.product_id;
+      // const productPrice = orderProduct.product_price;
+      const productQuantity = orderProduct.product_quantity;
+
+      const isExistProduct = await Products.findById(productId);
+
+      if (!isExistProduct) {
+        throw new ApiError(
+          httpStatus.NOT_FOUND,
+          'update time product is not found!'
+        );
+      }
+      await Products.findByIdAndUpdate(
+        productId,
+        {
+          $set: {
+            quantity: isExistProduct.quantity - productQuantity,
+          },
+        },
+        { session, new: true }
+      );
+    }
+
+    // ** [create a order]/part-02
+    // TODO: buyer is fake
+    // TODO: user_id will be update from auth middleware data
+    const result = await Orders.create(
+      [
+        {
+          buyer_id: '657ff528bcc34f2ba044d717',
+          user_id: '657ff528bcc34f2ba044d717',
+          order_code: order_code,
+          order_product_list: order_product_list,
+          total_items: order_product_list.length,
+          amount,
+          total_amount: amount,
+          shipment_address,
+        },
+      ],
+      { session, new: true }
+    );
+
+    await session.abortTransaction();
+    await session.endSession();
+
+    if (!result) {
+      throw new ApiError(httpStatus.BAD_REQUEST, 'Order is not created');
+    }
+    return result[0];
+  } catch (error) {
+    await session.abortTransaction();
+    await session.endSession();
+    throw error;
   }
-  return result;
 };
 
 export const OrdersServices = {
