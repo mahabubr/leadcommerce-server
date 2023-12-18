13fc301fea1671ca1826ec1d4e8abb5ef32537c8
Md Kamruzzaman <kamruzzaman-dev@users.noreply.github.com>
2023-12-18 10:39:16 +0600

    order interface and modal done

---
+++ src/app/modules/order/order.interface.ts
@@ -0,0 +1,46 @@
+import { Model, Types } from 'mongoose';
+import { IProducts } from '../products/products.interface';
+
+export type IOrdersProductList = {
+  product_id: Types.ObjectId | IProducts;
+  product_quantity: number;
+  product_price: number;
+};
+
+/* orders request for buyer */
+export type IOrdersReq = {
+  order_id: string;
+  user_Id: Types.ObjectId;
+  order_code: string; // automatically generated should be unique
+  order_product_list: IOrdersProductList[];
+  amount: number;
+  coupon_code: string;
+  coupon_discount: 200; // default should be 0
+  total_amount: number;
+};
+
+export type IOrdersFilters = {
+  searchTerm: string;
+};
+
+export type IOrderStatus = 'pending' | 'delivered' | 'cancel';
+
+/* orders */
+export type IOrders = {
+  buyer_id?: Types.ObjectId | string;
+  user_id: Types.ObjectId; // vendor user_id
+  order_product_list: IOrdersProductList[];
+  amount: number;
+  order_status: IOrderStatus;
+  coupon_code: string;
+  coupon_discount: number; // default should be 0
+  total_amount: number;
+  payment_id: Types.ObjectId;
+  payment_status: 'pending' | 'completed' | 'canceled';
+  payment_date?: string;
+  shipment_id?: Types.ObjectId;
+  shipment_status?: 'pending' | 'completed' | 'canceled';
+  shipment_date?: string;
+};
+
+export type OrdersModel = Model<IOrders, Record<string, unknown>>;
---
+++ src/app/modules/order/order.model.ts
@@ -0,0 +1,51 @@
+import { Schema, model } from 'mongoose';
+import { OrderStatus } from './order.constant';
+import { IOrders, OrdersModel } from './order.interface';
+
+const OrdersSchema = new Schema<IOrders, OrdersModel>({
+  buyer_id: { type: Schema.Types.ObjectId, default: null },
+  user_id: {
+    type: Schema.Types.ObjectId,
+    ref: 'Route',
+    required: true,
+  },
+  order_product_list: [
+    {
+      product_id: {
+        type: Schema.Types.ObjectId,
+        ref: 'Products',
+        required: true,
+      },
+      product_quantity: { type: Number, required: true },
+      product_price: { type: Number, required: true },
+    },
+  ],
+  amount: { type: Number, required: true },
+  order_status: {
+    type: String,
+    enum: OrderStatus,
+    default: 'pending',
+    required: true,
+  },
+  coupon_code: { type: String },
+  coupon_discount: { type: Number, default: 0, required: true },
+  total_amount: { type: Number, required: true },
+  payment_id: { type: Schema.Types.ObjectId },
+  payment_status: {
+    type: String,
+    enum: ['pending', 'completed', 'canceled'],
+    default: 'pending',
+    required: true,
+  },
+  payment_date: { type: String },
+  shipment_id: { type: Schema.Types.ObjectId },
+  shipment_status: {
+    type: String,
+    default: 'pending',
+    enum: ['pending', 'completed', 'canceled'],
+    required: true,
+  },
+  shipment_date: { type: String },
+});
+
+export const Orders = model<IOrders, OrdersModel>('Orders', OrdersSchema);
