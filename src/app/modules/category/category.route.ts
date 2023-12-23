23245fe9753e07af107a290e0c602bdabc13b4c0
mahabubr <mahabub.r753@gmail.com>
2023-12-23 22:18:45 +0600

    Authrozation Done in Category

--- src/app/modules/category/category.route.ts
+++ src/app/modules/category/category.route.ts
@@ -1,12 +1,22 @@
 import express from 'express';
+import { ENUM_ROLE } from '../../../enum/role';
+import auth from '../../middleware/auth';
 import { CatregoryController } from './category.controller';
 
 const router = express.Router();
 
-router.post('/', CatregoryController.createCategory);
-router.get('/', CatregoryController.getAllCategory);
-router.get('/:id', CatregoryController.getSingleCategory);
-router.patch('/:id', CatregoryController.updateCategory);
-router.delete('/:id', CatregoryController.deleteCategory);
+router.post('/', auth(ENUM_ROLE.ADMIN), CatregoryController.createCategory);
+router.get('/', auth(ENUM_ROLE.ADMIN), CatregoryController.getAllCategory);
+router.get(
+  '/:id',
+  auth(ENUM_ROLE.ADMIN),
+  CatregoryController.getSingleCategory
+);
+router.patch('/:id', auth(ENUM_ROLE.ADMIN), CatregoryController.updateCategory);
+router.delete(
+  '/:id',
+  auth(ENUM_ROLE.ADMIN),
+  CatregoryController.deleteCategory
+);
 
 export const CategoryRoutes = router;
